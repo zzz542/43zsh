98987a753baa8e8427586f3e3e345cd256f94f41
yap <2373308781@qq.com>
2024-11-20 14:13:57 +0800

    111

--- xscj/xscj/settings.py
+++ xscj/xscj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-em^d!ijwd*&d3kvdlstx0_a@4c_=rx=02e404q)er^d&tv4m)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -57,7 +57,8 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [os.path.join(BASE_DIR,'music/templates'),
-                 os.path.join(BASE_DIR,'templates')],
+                os.path.join(BASE_DIR,'accounts/templates'),
+                os.path.join(BASE_DIR,'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
